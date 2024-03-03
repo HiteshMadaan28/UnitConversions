53b01bafd3dcd4fa53d0fec200356980fd254e05
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-03-03 10:28:44 +0530

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Hitesh Madaan on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
